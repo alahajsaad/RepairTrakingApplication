810793dd63aebfcd98860382e42c1abeb8398d29
ala <alahajsaad398@gmail.com>
2025-01-28 11:08:18 +0100

    Running version with all basic requirements - 28/01/2025

--- ui/src/utils/Constants.tsx
+++ ui/src/utils/Constants.tsx
@@ -1,1 +1,5 @@
-export const BASE_URL = `http://localhost:8080/api`;
+const isLocal = window.location.hostname === 'localhost';
+
+export const BASE_URL = isLocal 
+  ? `http://localhost:8080/api` 
+  : `http://${window.location.hostname}:8080/api`;
